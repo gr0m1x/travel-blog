f562bfa6243705416e42a2bff4a2e6ff1ef34c30
gr0m1x <gromixaka@gmail.com>
2019-10-08 18:10:02 +0300

    facts section makeup

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
 
-    var swiper = new Swiper('.swiper-container', {
+    let swiperHeader = new Swiper('.swiper-header', {
         direction: 'vertical',
         effect: 'fade',
         autoplay: {
-            delay: 10000,
+            delay: 8000,
             disableOnInteraction: false,
         },
         pagination: {
@@ -12,26 +12,32 @@ $(document).ready(function () {
             // dynamicBullets: true,
             clickable: true,
         },
+    }),
+        swiperFatcs = new Swiper('.swiper-facts', {
+        slidesPerView: 3,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
     });
-
-    $("#scrollBtn").click(function() {
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $("header").outerHeight()
-        }, 1000);
-        return false;
-    });
-
-    // $(window).scroll(function() {
-    //     if ( $("#scrollBtn").offset().top >= window.pageYOffset) {
-    //         $("#scrollBtn").css("bottom", "100px")
-    //         console.log(true)
-    //     };
-    // });
 
     $(document)
     .on('click', ".hamburger_btn",function() {
         $("header").toggleClass('nav_open');
     })
+    .on("click","#scrollBtn, .navList_link__scrolling", function (event) {
+        let id  = $(this).attr('href'),
+            vwFix = ($(window).innerWidth() / 100) * 6,
+            top = $(id).offset().top + vwFix;
 
+        if ($('.nav_open').length) {
+            setTimeout(function () {
+                $("header").removeClass('nav_open');
+            }, 1000);
+        }
+
+        $('body,html').animate({scrollTop: top}, 1000);
+        return false;
+    });
 });
 
